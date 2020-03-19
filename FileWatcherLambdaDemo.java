618a4b9bc3e752e44a59a50d329c293687898b0d
WinIT23 <chauhanvinit23@gmail.com>
2020-03-20 01:07:13 +0530

    implemented Lambda in FileWatcher

--- FileWatcherLambdaDemo.java
+++ FileWatcherLambdaDemo.java
@@ -1,29 +1,48 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileWatcherLambdaDemo {
-    
+
     public static final int PAUSE_TIME = 500;
     private static final String SPLIT_REGEX = " ";
-    private static final List<String> fileList = new ArrayList<>(); 
-    public static void main(String[] args) {
-        
-        Arrays.stream(args)
-            .forEach(file -> fileList.add(file));
+    private static final List<String> fileList = new ArrayList<>();
 
-        fileList.forEach( file -> {
+    public static void main(String[] args) {
+
+        Arrays.stream(args)
+            .forEach(file -> {
+                FileWatcherLambdaDemo.fileList.add(file);
+            });
+
+        if (FileWatcherLambdaDemo.fileList.size() == 0) {
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+                Arrays.stream(br.readLine()
+                            .trim()
+                            .split(FileWatcherLambdaDemo.SPLIT_REGEX))
+                        .forEach(in -> fileList.add(in));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        fileList.forEach(file -> {
             new Thread(() -> {
                 File f = new File(file);
-                if(f.isFile()) {
+                if (f.isFile()) {
                     System.out.println("File " + file + " is Available..");
                 } else {
-                    System.out.println("File " + file + " is not Available.. <----------------"); 
+                    System.out.println("File " + file + " is not Available.. <----------------");
                 }
                 try {
                     Thread.sleep(FileWatcherLambdaDemo.PAUSE_TIME);
-                } catch(InterruptedException ex) {ex.printStackTrace();}
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
             }).start();
         });
     }
