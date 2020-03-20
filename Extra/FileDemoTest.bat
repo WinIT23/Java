3797d021234afa3728ebad99f1d6008cd59dfaff
WinIT23 <chauhanvinit23@gmail.com>
2020-03-20 19:32:42 +0530

    Added some new options in menu and used switchcase

--- Extra/FileDemoTest.bat
+++ Extra/FileDemoTest.bat
@@ -1,2 +1,2 @@
 javac com\file\example\FileDemo.java com\file\example\read\*Class.java com\file\example\write\*Class.java
-java com.file.example.FileDemo
+java com.file.example.FileDemo %1
--- Extra/com/file/example/FileDemo.java
+++ Extra/com/file/example/FileDemo.java
@@ -11,7 +11,9 @@ public class FileDemo {
 
     public static final int CHOICE_READ = 1,
                             CHOICE_WRITE = 2,
-                            CHOICE_CHANGE = 3;
+                            CHOICE_CHANGE = 3,
+                            CHOICE_MENU = 4,
+                            CHOICE_QUIT = 5;
 
     public static void main(String[] args) {
         String filePath = args.length != 0? args[0] : "";
@@ -19,18 +21,32 @@ public class FileDemo {
                                     new InputStreamReader(System.in))) {                   
             
             FileDemo.printMenu();
-            do {    
+            loop: do {    
                 System.out.println("Enter a choice : ");
                 int choice = Integer.parseInt(br.readLine().trim());
 
-                if(choice == FileDemo.CHOICE_READ) {
-                    FileReadClass.read(filePath);
-                } else if(choice == FileDemo.CHOICE_WRITE) {
-                    FileWriteClass.write(filePath);
-                } else {
-                    filePath = br.readLine().trim();
+                switch(choice) {
+                    case FileDemo.CHOICE_READ: 
+                        FileReadClass.read(filePath);
+                        break;
+                    case FileDemo.CHOICE_WRITE:
+                        FileWriteClass.write(filePath);
+                        break;
+                    case FileDemo.CHOICE_CHANGE:
+                        filePath = br.readLine().trim();
+                        break;
+                    case FileDemo.CHOICE_MENU:
+                        printMenu();
+                        continue loop;
+                    case FileDemo.CHOICE_QUIT:
+                        System.out.println("Bbyee!!! We will miss you...");
+                        break loop;
+                    default:
+                        System.out.println("Invalid Choice");
+                        continue loop;
                 }
-            } while (!isLeaving(br));
+
+            } while (true);
             
         } catch(IOException ex) {
             ex.printStackTrace();
@@ -38,9 +54,11 @@ public class FileDemo {
     }
 
     public static void printMenu() {
-        String s = "1. Read from the given file. \n" 
-                + "2. Write to the given file. \n" 
-                + "3. Change target file";
+        String s = "\t1. Read from the given file. \n" 
+                + "\t2. Write to the given file. \n" 
+                + "\t3. Change target file. \n" 
+                + "\t4. Print Menu. \n" 
+                + "\t5. Quit.";
 
         System.out.println(s);
     }
