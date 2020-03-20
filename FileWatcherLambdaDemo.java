7eed481384f8b35dc452b559aee96c2b19c88a92
WinIT23 <chauhanvinit23@gmail.com>
2020-03-20 14:22:22 +0530

    Added Scanner in case of no CMDline inputs

--- FileWatcherLambdaDemo.java
+++ FileWatcherLambdaDemo.java
@@ -6,24 +6,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class FileWatcherLambdaDemo {
+class FileWatcherLambda {
 
     public static final int PAUSE_TIME = 500;
     private static final String SPLIT_REGEX = " ";
     private static final List<String> fileList = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void watch(String[] args) {
 
         Arrays.stream(args)
             .forEach(file -> {
-                FileWatcherLambdaDemo.fileList.add(file);
+                FileWatcherLambda.fileList.add(file);
             });
 
-        if (FileWatcherLambdaDemo.fileList.size() == 0) {
+        if (FileWatcherLambda.fileList.size() == 0) {
             try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
                 Arrays.stream(br.readLine()
                             .trim()
-                            .split(FileWatcherLambdaDemo.SPLIT_REGEX))
+                            .split(FileWatcherLambda.SPLIT_REGEX))
                         .forEach(in -> fileList.add(in));
             } catch (IOException ex) {
                 ex.printStackTrace();
@@ -39,11 +39,17 @@ public class FileWatcherLambdaDemo {
                     System.out.println("File " + file + " is not Available.. <----------------");
                 }
                 try {
-                    Thread.sleep(FileWatcherLambdaDemo.PAUSE_TIME);
+                    Thread.sleep(FileWatcherLambda.PAUSE_TIME);
                 } catch (InterruptedException ex) {
                     ex.printStackTrace();
                 }
             }).start();
         });
+    }
+}
+
+public class FileWatcherLambdaDemo {
+    public static void main(String[] args) {
+        FileWatcherLambda.watch(args);
     }
 }
