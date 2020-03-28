050d510b5c9dbff5095913cb0ac36becded05977
WinIT23 <chauhanvinit23@gmail.com>
2020-03-28 18:22:48 +0530

    added benchmark in AddThread

--- Extra/com/thread/example/AddThread.java
+++ Extra/com/thread/example/AddThread.java
@@ -2,10 +2,11 @@ package com.thread.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 @FunctionalInterface
 interface SumChunk {
-    void run(long startRange, long endRange);
+    long run(long startRange, long endRange);
 }
 
 @FunctionalInterface
@@ -15,45 +16,68 @@ interface SumThreads {
 
 public class AddThread {
 
-    public static final int THREAD_COUNT = 8;
+    public static int THREAD_COUNT = 4;
+    public static long sumTill = 10;
 
     private long sum;
-    private long sumTill = Integer.MAX_VALUE;
     private List<Long> answers = new ArrayList<>();
+
+    AddThread() {
+        this(4, 10);
+        System.out.println("using default values (4, 10)");
+    }
+
+    AddThread(int threadCount, long endRange) {
+        AddThread.THREAD_COUNT = threadCount;
+        AddThread.sumTill = endRange;
+    }
 
     private SumThreads tRun = () -> {
         // Adds the answers of all threads.
         long[] x = new long[1];
         x[0] = answers.stream().filter(i -> true).mapToLong(i -> i).sum();
-        System.out.println(this.answers);
         return x[0];
     };
 
     private SumChunk sRun = (s, e) -> {
         // adds the part of the range.
-        new Thread(() -> {
+        // new Thread(() -> {
+        Callable callable = () -> {
             long tSum = 0;
-            for (long i = s; i < e; i++) {
+            for (long i = s; i <= e; i++) {
                 tSum += i;
             }
-            answers.add(tSum);
-            System.out.println("tSum : " + tSum 
-                            + " aList : " + answers.get(answers.size() - 1));
-        }).start();
+            // System.out.println("S : " + s + " tSum : " + tSum
+            // + " aList : " + answers.get(answers.size() - 1));
+            return tSum;
+        };
+        try {
+            return (Long) callable.call();
+        } catch (Exception ex) {
+            System.out.println("Exception at sRun");
+            ex.printStackTrace();
+            return 0;
+        }
+        // }).start();
     };
 
     private long divideTask(long perThread) {
         for (int i = 0; i < AddThread.THREAD_COUNT; i++) {
-            sRun.run(perThread * i, (perThread * (i + 1)) - 1);
-            // System.out.println(i + " : " + perThread*i);
+            long s = perThread * i,
+                    e = (i == AddThread.THREAD_COUNT - 1) ? AddThread.sumTill : (perThread * (i + 1)) - 1;
+
+            long tSum = sRun.run(s, e);
+            // System.out.println(i + " : " + s + "\t" + e + "\t" + tSum);
+            answers.add(tSum);
         }
-        System.out.println(answers);
+        //System.out.println(answers);
         return tRun.run();
     }
 
-    public void calculate() {
-        long perThread = sumTill / AddThread.THREAD_COUNT;
-        sum = this.divideTask(perThread);
-        System.out.println("Sum is : " + sum);
+    public long calculate() {
+        long perThread = AddThread.sumTill / AddThread.THREAD_COUNT;
+        this.sum = this.divideTask(perThread);
+        return this.sum;
+        // System.out.println("Sum is : " + sum);
     }
 }
--- Extra/com/thread/example/AddThreadDemo.java
+++ Extra/com/thread/example/AddThreadDemo.java
@@ -1,8 +1,32 @@
 package com.thread.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddThreadDemo {
-    public static void main(String[] args) {
-        AddThread tObj = new AddThread();
+    public static double test() {
+        long startTime = System.nanoTime();
+
+        AddThread tObj = new AddThread(13, Integer.MAX_VALUE);
         tObj.calculate();
+
+        double elapsedTime = (System.nanoTime() - startTime)/1000000;
+     
+        System.out.println("Total execution time in milis: " + elapsedTime);
+        return elapsedTime;
     }
+
+    public static void main(String[] args) {
+
+        int test = Integer.parseInt(args[0]);
+        List<Double> times = new ArrayList<>();
+
+        for(int i = 0; i < test; i++) {
+            times.add(AddThreadDemo.test());
+        }
+
+        double avgTime = times.stream().filter(i -> true).mapToDouble(i -> i).sum() / test;
+        
+        System.out.println("Average time : " + avgTime);
+    } 
 }
